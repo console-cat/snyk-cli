75e4238394be7348bd4d97c44d4ce18b0551917c
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2022-07-19 11:47:20 +0200

    chore: attempt to fix tests on ci
    making test location and container name agnostic
    
    Signed-off-by: Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>

--- cliv2/test/acceptance/kerberos.spec.ts
+++ cliv2/test/acceptance/kerberos.spec.ts
@@ -5,12 +5,13 @@ import {
   TestProject,
 } from '../../../test/jest/util/createProject';
 import { startCommand } from '../../../test/jest/util/startSnykCLI';
+import { isCLIV2 } from '../../../test/jest/util/isCLIV2';
 
 jest.setTimeout(1000 * 60);
 
 // Global test configuration
 const dockerComposeFile = path.resolve(
-  'test/fixtures/kerberos/docker-compose.yml',
+  path.join(__dirname, '..', 'fixtures', 'kerberos', 'docker-compose.yml'),
 );
 const containerName = 'kerberos_cliv2_kerberos_1';
 const hostnameFakeServer = 'host.docker.internal';
@@ -29,6 +30,7 @@ function getDockerOptions(projectPath = '') {
       PROJECT_PATH: projectPath,
       PROXY_HOSTNAME: hostnameProxy,
       SNYK_API: SNYK_API,
+      CONTAINER_NAME: containerName,
     },
   };
   return dockerOptions;
@@ -59,7 +61,7 @@ function getDockerExecSnykCommand(env: Record<string, string>, cmd = 'test') {
 
 async function startKerberosEnvironment(project: TestProject): Promise<void> {
   // Stop any orphaned containers from previous runs.
-  await stopKerberosEnvironment();
+  await stopKerberosEnvironment(project);
 
   const dockerUp = await startCommand(
     'docker-compose',
@@ -71,11 +73,11 @@ async function startKerberosEnvironment(project: TestProject): Promise<void> {
   });
 }
 
-async function stopKerberosEnvironment(): Promise<void> {
+async function stopKerberosEnvironment(project: TestProject): Promise<void> {
   const dockerDown = await startCommand(
     'docker-compose',
     ['--file', dockerComposeFile, 'down'],
-    getDockerOptions(),
+    getDockerOptions(project.path()),
   );
   await expect(dockerDown).toExitWith(0, { timeout: 30_000 });
 }
@@ -91,79 +93,79 @@ async function getAccessLog(): Promise<string> {
   return check.stdout.get();
 }
 
-describe('kerberos', () => {
-  if (!process.env.TEST_SNYK_COMMAND?.includes('linux')) {
+describe('kerberos proxy authentication', () => {
+  if (!process.env.TEST_SNYK_COMMAND?.includes('linux') || !isCLIV2()) {
     // eslint-disable-next-line jest/no-focused-tests
-    it.only('Kerberos can only be tested against Linux builds.', () => {
+    it.only('These tests are currently limited to cover linux builds of CLIv2.', () => {
       console.warn(
-        'Skipping test. Kerberos can only be tested against Linux builds.',
+        'Skipping test. These tests are currently limited to cover linux builds of CLIv2.',
       );
     });
+  } else {
+    let server: FakeServer;
+    let env: Record<string, string>;
+    let project: TestProject;
+
+    beforeAll(async () => {
+      project = await createProjectFromWorkspace('npm-package');
+      await startKerberosEnvironment(project);
+
+      env = {
+        ...process.env,
+        SNYK_API: SNYK_API,
+        SNYK_TOKEN: '123456789',
+        HTTP_PROXY: HTTP_PROXY,
+      };
+      server = fakeServer(baseApi, env.SNYK_TOKEN);
+      await server.listenPromise(port);
+    });
+
+    afterEach(() => {
+      server.restore();
+    });
+
+    afterAll(async () => {
+      await server.closePromise();
+      await stopKerberosEnvironment(project);
+    });
+
+    it('fails to run snyk test due to missing --proxy-negotiate', async () => {
+      // How to get project fixtures into docker container?
+      // - Allow function to take output path instead of tmp dir.
+      // - Basically, nested workspaces.
+      const logOnEntry = await getAccessLog();
+
+      // run snyk test
+      const cli = await startCommand('docker', getDockerExecSnykCommand(env));
+      await expect(cli).toExitWith(2);
+
+      const logOnExit = await getAccessLog();
+      const additionalLogEntries = logOnExit.substring(logOnEntry.length);
+      expect(additionalLogEntries.includes('TCP_DENIED/407')).toBeTruthy();
+      expect(
+        additionalLogEntries.includes(
+          'CONNECT ' + hostnameFakeServer + ':' + port,
+        ),
+      ).toBeFalsy();
+    });
+
+    it('successfully runs snyk test', async () => {
+      const logOnEntry = await getAccessLog();
+
+      // run snyk test
+      const cmd = getDockerExecSnykCommand(env);
+      cmd.push('--proxy-negotiate');
+      const cli = await startCommand('docker', cmd);
+      await expect(cli).toExitWith(0);
+
+      const logOnExit = await getAccessLog();
+      const additionalLogEntries = logOnExit.substring(logOnEntry.length);
+      expect(additionalLogEntries.includes('TCP_TUNNEL/200')).toBeTruthy();
+      expect(
+        additionalLogEntries.includes(
+          'CONNECT ' + hostnameFakeServer + ':' + port,
+        ),
+      ).toBeTruthy();
+    });
   }
-
-  let server: FakeServer;
-  let env: Record<string, string>;
-  let project: TestProject;
-
-  beforeAll(async () => {
-    project = await createProjectFromWorkspace('npm-package');
-    await startKerberosEnvironment(project);
-
-    env = {
-      ...process.env,
-      SNYK_API: SNYK_API,
-      SNYK_TOKEN: '123456789',
-      HTTP_PROXY: HTTP_PROXY,
-    };
-    server = fakeServer(baseApi, env.SNYK_TOKEN);
-    await server.listenPromise(port);
-  });
-
-  afterEach(() => {
-    server.restore();
-  });
-
-  afterAll(async () => {
-    await server.closePromise();
-    await stopKerberosEnvironment();
-  });
-
-  it('fails to run snyk test due to missing --proxy-negotiate', async () => {
-    // How to get project fixtures into docker container?
-    // - Allow function to take output path instead of tmp dir.
-    // - Basically, nested workspaces.
-    const logOnEntry = await getAccessLog();
-
-    // run snyk test
-    const cli = await startCommand('docker', getDockerExecSnykCommand(env));
-    await expect(cli).toExitWith(2);
-
-    const logOnExit = await getAccessLog();
-    const additionalLogEntries = logOnExit.substring(logOnEntry.length);
-    expect(additionalLogEntries.includes('TCP_DENIED/407')).toBeTruthy();
-    expect(
-      additionalLogEntries.includes(
-        'CONNECT ' + hostnameFakeServer + ':' + port,
-      ),
-    ).toBeFalsy();
-  });
-
-  it('successfully runs snyk test', async () => {
-    const logOnEntry = await getAccessLog();
-
-    // run snyk test
-    const cmd = getDockerExecSnykCommand(env);
-    cmd.push('--proxy-negotiate');
-    const cli = await startCommand('docker', cmd);
-    await expect(cli).toExitWith(0);
-
-    const logOnExit = await getAccessLog();
-    const additionalLogEntries = logOnExit.substring(logOnEntry.length);
-    expect(additionalLogEntries.includes('TCP_TUNNEL/200')).toBeTruthy();
-    expect(
-      additionalLogEntries.includes(
-        'CONNECT ' + hostnameFakeServer + ':' + port,
-      ),
-    ).toBeTruthy();
-  });
 });
