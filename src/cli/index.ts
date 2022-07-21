2e46b8a1962045f699265b6d4c4baae7da2ad0a5
Matt Evenson <mevenson@usc.edu>
2022-07-20 17:07:59 -0700

    add console cat

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,6 +1,13 @@
 import { callHandlingUnexpectedErrors } from '../lib/unexpected-error';
 import { EXIT_CODES } from './exit-codes';
+import { ConsoleCat } from '@console-cat/sdk';
 
+ConsoleCat.initialize({
+  cliId: 'snyk-b96457fb-8457-4473-8251-0929e101c465',
+  apiUrl: 'http://localhost:8080',
+  packageJson: require('../../package.json'),
+  debug: true,
+});
 /**
  * By using a dynamic import, we can add error handlers before evaluating any
  * further modules. This way, if a module has errors, it'll be caught and
